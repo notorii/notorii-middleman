6f55bd395827deb273784b2e5a2ee7a33077cee4
Harold Herbert <haroldherbert@me.com>
2014-03-07 13:18:35 +1100

    thumbnail update, favicon, PDF link update

--- Gemfile
+++ Gemfile
@@ -7,6 +7,9 @@ gem "middleman", "~>3.2.1"
 # Live-reloading plugin
 gem "middleman-livereload", "~> 3.1.0"
 
+# favicon generator
+gem "middleman-favicon-maker", "~> 3.5"
+
 # For faster file watcher updates on Windows:
 gem "wdm", "~> 0.1.0", :platforms => [:mswin, :mingw]
 
--- config.rb
+++ config.rb
@@ -60,11 +60,23 @@ configure :build do
   # activate :minify_javascript
 
   # Enable cache buster
-  # activate :asset_hash
+  activate :asset_hash
 
   # Use relative URLs
-  # activate :relative_assets
+  activate :relative_assets
 
   # Or use a different image path
   # set :http_prefix, "/Content/images/"
+
+  activate :favicon_maker, :icons => {
+    "favicon.png" => [
+      { icon: "apple-touch-icon-152x152-precomposed.png" },
+      { icon: "apple-touch-icon-114x114-precomposed.png" },
+      { icon: "apple-touch-icon-72x72-precomposed.png" },
+      { icon: "favicon.png", size: "16x16" },
+      { icon: "favicon.ico", size: "64x64,32x32,24x24,16x16" },
+      { icon: "mstile-144x144", format: "png" },
+    ]
+  }
+
 end
